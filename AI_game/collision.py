6b05ad84e426bca5a2cfef2292ec094890b80d15
Book-Shelf <arthuradd@yahoo.com>
2023-11-05 01:49:07 +0100

    Added simple colision detection for player

--- AI_game/collision.py
+++ AI_game/collision.py
@@ -14,19 +14,16 @@ class CollisionDetection:
         for obstacle in self.obstacles:
             for enemy in self.enemies:
                 distance = obstacle.getPos() - enemy.getPos() 
-                if distance.length() < obstacle.radius + enemy.radius:
-                    if distance.length() != 0:
-                        overlap = (obstacle.radius + enemy.radius) - distance.length()
-                        overlap_vector = distance.normalize() * overlap
-                        enemy.pos -= overlap_vector
+                if distance.length() != 0 and distance.length() < obstacle.radius + enemy.radius:
+                    overlap = (obstacle.radius + enemy.radius) - distance.length()
+                    overlap_vector = distance.normalize() * overlap
+                    enemy.pos -= overlap_vector
         #     # Calculate the distance vector between the player and the obstacle
-        #     distance = pygame.math.Vector2(obstacle.x - self.player.x, obstacle.y - self.player.y)
-        #     if distance.length() < obstacle.radius + self.player.radius:
-        #         # Check if the distance is less than the sum of the radii (collision detected)
-        #         overlap = (obstacle.radius + self.player.radius) - distance.length()
-        #         if distance.length() != 0:
-        #             # Prevent division by zero if the distance is zero
-        #             overlap_vector = distance.normalize() * overlap
-        #             # Calculate the overlap vector to resolve the collision
-        #             self.player.x -= overlap_vector.x  # Adjust the player's x-coordinate
-        #             self.player.y -= overlap_vector.y  # Adjust the player's y-coordinate
+            distance = obstacle.getPos() - self.player.pos
+            if distance.length() != 0 and distance.length() < obstacle.radius + self.player.size:
+                # Check if the distance is less than the sum of the radii (collision detected)
+                overlap = (obstacle.radius + self.player.size) - distance.length()
+                # Prevent division by zero if the distance is zero
+                overlap_vector = distance.normalize() * overlap
+                # Calculate the overlap vector to resolve the collision
+                self.player.pos -= overlap_vector 
